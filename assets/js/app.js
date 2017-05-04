5620e46338e76c692b24524f5a195cfaf6de213f
soundestmammal <robert.checco@gmail.com>
2017-05-04 11:56:43 -0400

    Got new list items to be able to line-through and fade out

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,21 @@
 //Check Off Specific Todos by Clicking
-$("li").click(function(){
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
 });
 
 //Click on X to delete todo
-$("span").click(function(event){
+$("ul").on("click",  "span" , function(event){
 	$(this).parent().fadeOut(500 , function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		var todoText = $(this).val();
+		$("ul").append('<li>' + '<span>X </span>' + todoText + '</li>');
+		$(this).val("");		
+	}
 	
 });
