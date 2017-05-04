9c4d01a1fe06bb5aa6119e448dfbe690ee826e44
soundestmammal <robert.checco@gmail.com>
2017-05-04 12:36:21 -0400

    Add trash can

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,7 +14,7 @@ $("ul").on("click",  "span" , function(event){
 $("input[type='text']").keypress(function(event){
 	if(event.which === 13){
 		var todoText = $(this).val();
-		$("ul").append('<li>' + '<span>X </span>' + todoText + '</li>');
+		$("ul").append('<li>' + '<span><i class="fa fa-trash"></i></span>' + todoText + '</li>');
 		$(this).val("");		
 	}
 	
